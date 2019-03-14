671f475f77c076936ee44702ad6afcc2934191e4
timor <caojiurui333@gmail.com>
2019-03-14 13:48:51 +0800

    version 1

---
+++ Love_files/index.js
@@ -0,0 +1,12 @@
+(function(window){
+
+    $(function () {
+        $('#myLink').click(function () {
+            alert('别点啦,我知道你喜欢我')
+        })
+        $('#sheLink').click(function () {
+            alert('别碰我漂亮单纯善良的小贝子')
+        })
+    })
+
+})(window)
